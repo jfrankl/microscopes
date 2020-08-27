2ca78d1fb436a443261903b4ad42f3ca5da29e8f
jfrankl <jfrankl@azavea.com>
2020-08-27 13:48:57 -0400

    Initial add

---
+++ overlay.js
@@ -0,0 +1,24 @@
+function init() {
+
+  var Esri_WorldImagery = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+  attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+  });
+
+  var map = new L.Map('map', {
+    center: [50.0, 10.0],
+    zoom: 5,
+    minZoom: 5,
+    maxZoom: 10,
+    layers: [
+      Esri_WorldImagery
+    ]
+  });
+
+  var circle = document.getElementById('circle');
+
+  map.on('zoomanim', function(e) {
+      circle.style.transform = `scale(${e.target._animateToZoom - 4})`
+  })
+}
+
+window.onload = init;
