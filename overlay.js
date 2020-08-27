98e3b28649fe33e1114c48ba2a2fbad8be479314
jfrankl <jfrankl@azavea.com>
2020-08-27 16:57:15 -0400

    Update overlay on keyboard

--- overlay.js
+++ overlay.js
@@ -7,7 +7,7 @@ function init() {
     }
   );
 
-  var map = new L.Map("map", {
+  window.map = new L.Map("map", {
     center: [50.0, 10.0],
     zoom: 5,
     minZoom: 5,
@@ -17,11 +17,52 @@ function init() {
 
   var circle = document.getElementById("circle");
 
+  var initialZoom;
+
+  var initialCenter;
+
+  var keyDown = false;
+
+  function zoomTo(zoom) {
+    circle.style.transform = `scale(${Math.pow(2, zoom - 5)})`;
+  }
+
   map.on("zoomanim", function(e) {
-    circle.style.transform = `scale(${Math.pow(
-      2,
-      e.target._animateToZoom - 5
-    )})`;
+    zoomTo(e.target._animateToZoom);
+  });
+
+  map.on("click", function(e) {
+    if (keyDown === true) {
+      var latlng = e.latlng;
+      initialZoom = map.getZoom();
+      initialCenter = map.getCenter();
+      document.body.classList.add("mod");
+      var autoZoom = 10;
+      zoomTo(autoZoom);
+      console.log(latlng);
+      map.setView(latlng, autoZoom);
+    }
+  });
+
+  document.addEventListener("keydown", function(e) {
+    document.body.classList.add("active");
+    if (e.keyCode === 83) {
+      if (!keyDown) {
+        keyDown = true;
+      }
+    }
+  });
+
+  document.addEventListener("keyup", function(e) {
+    document.body.classList.remove("active");
+    if (e.keyCode === 83) {
+      keyDown = false;
+      zoomTo(initialZoom);
+      document.body.classList.remove("mod");
+      initialZoom && initialCenter && map.setView(initialCenter, initialZoom);
+      initialCenter = undefined;
+      initialZoom = undefined;
+    }
   });
 }
 
