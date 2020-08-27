e1208f238b9954db36d0a4039fc7fab9d4837ae1
jfrankl <jfrankl@azavea.com>
2020-08-27 16:12:22 -0400

    Update zoom levels

--- overlay.js
+++ overlay.js
@@ -1,24 +1,28 @@
 function init() {
+  var Esri_WorldImagery = L.tileLayer(
+    "https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}",
+    {
+      attribution:
+        "Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community"
+    }
+  );
 
-  var Esri_WorldImagery = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
-  attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
-  });
-
-  var map = new L.Map('map', {
+  var map = new L.Map("map", {
     center: [50.0, 10.0],
     zoom: 5,
     minZoom: 5,
     maxZoom: 10,
-    layers: [
-      Esri_WorldImagery
-    ]
+    layers: [Esri_WorldImagery]
   });
 
-  var circle = document.getElementById('circle');
+  var circle = document.getElementById("circle");
 
-  map.on('zoomanim', function(e) {
-      circle.style.transform = `scale(${e.target._animateToZoom - 4})`
-  })
+  map.on("zoomanim", function(e) {
+    circle.style.transform = `scale(${Math.pow(
+      2,
+      e.target._animateToZoom - 5
+    )})`;
+  });
 }
 
 window.onload = init;
